9ccc2f242fa8de877e49de278f12d988a73ff893
RichardTscharke <r.tscharke@proton.me>
2026-01-29 14:02:42 +0100

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,12 +6,16 @@ from evaluation.scripts.confusion_matrix import plot_confusion_matrix
 def main():
 
     calculate_inference(MODEL_PATH = "models/model1_v0.pth")
+    print(f"[INFO] Inference calculated.")
 
     plot_epoch_curves(csv_path = "src/evaluation/outputs/epoch_metrics.csv")
+    print(f"[INFO] Epoch curves drawn.")
 
     plot_prec_recall_f1_p_class()
+    print(f"[INFO] Precision, Recall and F1 per class diagramm created.")
 
     plot_confusion_matrix(normalized = True)
+    print(f"[INFO] Confusion Matrix drawn.")
 
 
 if __name__ == "__main__":
